7c84d5ff57799d8ab8e0779db77e253dfc126f12
Karim El Jed <karim@eljed.de>
2024-01-14 20:22:14 +0100

    Generate use case documentation with embeded use case diagram

--- DomainDrivenDesign.DiagramGenerators.Console/Program.cs
+++ DomainDrivenDesign.DiagramGenerators.Console/Program.cs
@@ -9,10 +9,12 @@ var formatter = DocumentationFormatter.ForDefaultMarkdown();
 var generator = new DocumentationGenerator(provider, formatter);
 
 var documentation = generator.DocumentUseCase(typeof(UseCaseWithMultipleActors));
-
 File.WriteAllText("documentation.md", documentation);
-
 Console.WriteLine(documentation);
+
+var documentation2 = generator.DocumentUseCase(typeof(UseCaseWithOneActor));
+File.WriteAllText("documentation2.md", documentation2);
+Console.WriteLine(documentation2);
 
 //var generator = new UseCaseDiagramGenerator(assembly, true);
 //var diagram = generator.CreateDiagramForAllUseCases();
